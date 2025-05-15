6302574f04768aa811f02114f51e271b842e4b7c
Freek <fpw.straten@student.avans.nl>
2025-05-15 13:04:46 +0200

    test

--- scripts/init-data.js
+++ scripts/init-data.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb'); //hi
 
 async function initData() {
     const uri = process.env.MONGO_URL || 'mongodb://mijn-mongo:27017';
